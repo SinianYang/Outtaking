218e8a62572f699d4d896122db307dfec90a02ef
QHZ_SS <479754908@qq.com>
2018-06-13 17:55:57 +0800

    compelete location

--- app/src/main/java/com/sjtu/outtaking/MainActivity.java
+++ app/src/main/java/com/sjtu/outtaking/MainActivity.java
@@ -1,9 +1,16 @@
 package com.sjtu.outtaking;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -17,14 +24,26 @@ import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private MapView mMapView = null;
+    private LocationManager locationManager;
+    private String provider;
+    BaiduMap baiduMap;
+    boolean ifFrist = true;
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -37,10 +56,47 @@ public class MainActivity extends AppCompatActivity {
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
         mMapView = findViewById(R.id.bmapView);
+
+
+        // 获取baiduMap对象
+        baiduMap = mMapView.getMap();
+        // 设置可改变地图位置
+        baiduMap.setMyLocationEnabled(true);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        List<String> list = locationManager.getProviders(true);
+        if (list.contains(LocationManager.GPS_PROVIDER)) {
+            provider = LocationManager.GPS_PROVIDER;
+        } else if (list.contains(LocationManager.NETWORK_PROVIDER)) {
+            provider = LocationManager.NETWORK_PROVIDER;
+
+        } else {
+            Toast.makeText(this, "当前不能提供位置信息", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+        if (location != null) {
+            navigateTo(location);
+        }
+        locationManager.requestLocationUpdates(provider, 5000, 1, locationListener);
+
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ListView listView=(ListView)findViewById(R.id.left_drawer);
         String[] mMenuTitles=getResources().getStringArray(R.array.menu_drawer_list);
+
+
         try{
             ArrayAdapter<String> arrayAdapter=new ArrayAdapter<>(this,R.layout.drawer_list_item,mMenuTitles);
             listView.setAdapter(arrayAdapter);
@@ -52,6 +108,57 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    private void navigateTo(Location location) {
+        // 按照经纬度确定地图位置
+        if (ifFrist) {
+            LatLng ll = new LatLng(location.getLatitude(),
+                    location.getLongitude());
+            MapStatusUpdate update = MapStatusUpdateFactory.newLatLng(ll);
+            // 移动到某经纬度
+            baiduMap.animateMapStatus(update);
+            update = MapStatusUpdateFactory.zoomBy(5f);
+            // 放大
+            baiduMap.animateMapStatus(update);
+
+            ifFrist = false;
+        }
+        // 显示个人位置图标
+        MyLocationData.Builder builder = new MyLocationData.Builder();
+        builder.latitude(location.getLatitude());
+        builder.longitude(location.getLongitude());
+        MyLocationData data = builder.build();
+        baiduMap.setMyLocationData(data);
+    }
+
+    LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onProviderEnabled(String arg0) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onProviderDisabled(String arg0) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onLocationChanged(Location arg0) {
+            // TODO Auto-generated method stub
+            // 位置改变则重新定位并显示地图
+            navigateTo(arg0);
+        }
+    };
+
 
     private void selectItem(int position){
 
